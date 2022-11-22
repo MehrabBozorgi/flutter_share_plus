b4cade5d3ff51ea314955f423d7f515af2ac067d
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-11-22 17:45:11 +0330

    share_plus

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
+  }
+}
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Share Plus'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Share link'),
+          onPressed: (){
+
+            Share.share('www.mehrabbozorgi.ir',
+            subject: 'Check out the website'
+            );
+
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
